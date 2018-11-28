950f3f941fb7939387cb270e153e6e592098ba24
Sebastien Bovo <sebastienbovo@outlook.com>
2018-11-28 09:50:58 +0100

    Add UnitTestApp with tests

--- src/UnitTestApp/UWPPasswordVaultHelperTest_GET.cs
+++ src/UnitTestApp/UWPPasswordVaultHelperTest_GET.cs
@@ -7,10 +7,10 @@ using Windows.Security.Credentials;
 namespace UnitTestApp
 {
     [TestClass]
-    public class UWPPasswordVaultHelperTest
+    public class UWPPasswordVaultHelperTest_GET
     {
         [TestMethod]
-        public void GetPasswordCredential_EmptyResource_shouldReturnNull()
+        public void EmptyResource_shouldReturnNull()
         {
             PasswordVaultHelper pvh = new PasswordVaultHelper();
             PasswordCredential pc = pvh.GetPasswordCredential(null, "SampleUserName");
@@ -18,7 +18,7 @@ namespace UnitTestApp
         }
 
         [TestMethod]
-        public void GetPasswordCredential_EmptyUsername_shouldReturnNull()
+        public void EmptyUsername_shouldReturnNull()
         {
             PasswordVaultHelper pvh = new PasswordVaultHelper();
             PasswordCredential pc = pvh.GetPasswordCredential("SampleResource", null);
@@ -26,19 +26,20 @@ namespace UnitTestApp
         }
 
         [TestMethod]
-        public void WritePasswordCredential_EmptyPassword_shouldReturnNull()
+        public void Username_shouldPasswordCredential()
         {
+            string resource = "SampleResourceForGet";
+            string username = "UserID";
+            string password = "secret";
             PasswordVaultHelper pvh = new PasswordVaultHelper();
-            bool result = pvh.WritePasswordCredential("SampleResource", "ABC", null);
-            Assert.IsFalse(result);
+            bool result = pvh.WritePasswordCredential(resource, username, password);
+
+            PasswordVaultHelper pvhget = new PasswordVaultHelper();
+            PasswordCredential pc = pvhget.GetPasswordCredential(resource,username);
+            bool success = pc.Password.CompareTo(password) == 0;
+            Assert.IsTrue(success);
         }
 
-        [TestMethod]
-        public void WritePasswordCredential_shouldReturnTrue()
-        {
-            PasswordVaultHelper pvh = new PasswordVaultHelper();
-            bool result = pvh.WritePasswordCredential("SampleResource", "1234", "secrect");
-            Assert.IsTrue(result);
-        }
+        
     }
 }
---
+++ src/UnitTestApp/UWPPasswordVaultHelperTest_WRITE.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UWP_PasswordVault_Helper;
+using Windows.Security.Credentials;
+
+namespace UnitTestApp
+{
+    [TestClass]
+    public class UWPPasswordVaultHelperTest_WRITE
+    {
+        [TestMethod]
+        public void WritePasswordCredential_EmptyPassword_shouldReturnNull()
+        {
+            PasswordVaultHelper pvh = new PasswordVaultHelper();
+            bool result = pvh.WritePasswordCredential("SampleResource", "ABC", null);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void WritePasswordCredential_shouldReturnTrue()
+        {
+            PasswordVaultHelper pvh = new PasswordVaultHelper();
+            bool result = pvh.WritePasswordCredential("SampleResource", "1234", "secret");
+            Assert.IsTrue(result);
+        }
+    }
+}
